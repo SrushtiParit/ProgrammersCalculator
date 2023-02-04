689bf420b5c42c5e9be90c8d6c71e7b8c877c417
Srushti Parit <102237808+SrushtiParit@users.noreply.github.com>
2023-02-04 11:04:11 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+/**
+ *
+ * @author Srushti Parit
+ */
+public class Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FirstPage fp = new FirstPage();
+        fp.setVisible(true);
+        fp.setSize(800, 700);
+        fp.setLocation(100, 100);
+    }
+    
+}
